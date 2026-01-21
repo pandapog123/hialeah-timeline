236c87c1a87e32596d9112d83b5d56225ec6efa9
Daniel Gorra <danielgorra7@gmail.com>
2026-01-21 09:19:17 -0500

    Scaffold for Success Contact Page

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,1 +1,144 @@
-<h1> FAQ </h1>
+<script lang="ts">
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+
+  let currentFAQ: number | undefined = $state();
+</script>
+
+<section class="faq">
+  <h1>Frequently Asked Questions</h1>
+
+  <ul class="faq-accordion">
+    {#each data.faqs as faq, i}
+      <li class:selected={i === currentFAQ}>
+        <button
+          onclick={() => {
+            if (currentFAQ === i) {
+              currentFAQ = undefined;
+            } else {
+              currentFAQ = i;
+            }
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+
+          <p class="accordion-title">{faq.title}</p>
+        </button>
+
+        <div class="accordion-content" class:hidden={i !== currentFAQ}>
+          <div class="accordion-content-inner">
+            <div class="accordion-content-padding">
+              {faq.content}
+            </div>
+          </div>
+        </div>
+      </li>
+
+      {#if i !== data.faqs.length - 1}
+        <hr />
+      {/if}
+    {/each}
+  </ul>
+</section>
+
+<style>
+  * {
+    margin: 0;
+  }
+
+  section {
+    padding: 1rem;
+    padding-bottom: 10rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  ul {
+    padding: 0;
+    list-style-type: none;
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    padding: 1rem;
+    font-size: 1rem;
+
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    flex: 1;
+  }
+
+  .faq-accordion {
+    overflow: hidden;
+    border-radius: 0.5rem;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+
+    transition: transform 200ms ease-in-out;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .accordion-title {
+    text-align: left;
+  }
+
+  li.selected svg {
+    transform: rotate(90deg);
+  }
+
+  hr {
+    border: none;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  .accordion-content {
+    display: grid;
+    grid-template-rows: 1fr;
+
+    transition: all 250ms ease-in-out;
+  }
+
+  .accordion-content.hidden {
+    grid-template-rows: 0fr;
+  }
+
+  .accordion-content-inner {
+    overflow: hidden;
+  }
+
+  .accordion-content-padding {
+    padding: 1rem;
+    padding-top: 0;
+  }
+
+  @media (min-width: 650px) {
+    .accordion-title {
+      font-size: 1.25rem;
+    }
+  }
+</style>
--- src/routes/faq/+page.ts
+++ src/routes/faq/+page.ts
@@ -2,8 +2,47 @@ import type { PageLoad } from "./$types";
 
 export const prerender = true;
 
+interface FAQElement {
+  title: string;
+  content: string;
+}
+
 export const load: PageLoad = async () => {
+  const FAQs: FAQElement[] = [
+    {
+      title: "How do I get in contact?",
+      content:
+        "fjdsakl;fjdskla;fjdklsa; fdlksajfl fjdaskl;f fdjsakl; fjkl;as ffjf fjdksla fjfds f fds dsa dfsa fds afd s f dsaf dsa fdsa fd safdsa fdsa f ds",
+    },
+    {
+      title: "How do I find events happening in Hialeah or Hialeah Gardens?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+    {
+      title: "Where can I find free food for my family?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+    {
+      title:
+        "Do I need to be registered or show proof of finances to qualify for food aid?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+    {
+      title: "How can I volunteer at food drives or events?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+    {
+      title: "Can I request a local resource to be added to this site?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+    {
+      title: "Can I request a local event to be added to this site?",
+      content: "fjdsakl;fjdskla;fjdklsa;",
+    },
+  ];
+
   return {
-    title: "FAQ"
-  }
+    title: "FAQ",
+    faqs: FAQs,
+  };
 };
